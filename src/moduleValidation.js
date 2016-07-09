aa11fa78e5d398dd59c64512837703344cce55a1
Orestis Mavropoulos <orestis@batCave.local>
2016-07-10 00:21:47 +0100

    validation module

--- src/moduleValidation.js
+++ src/moduleValidation.js
@@ -5,59 +5,74 @@
 // needs refactoring once the metamodel is finished
 
 module.exports = function moduleValidation (s) {
-  deviceValidation(s)
+  componentValidation(s, 'device', deviceArray)
+  componentValidation(s, 'network connection', networkArray)
+  componentValidation(s, 'microcosm', microcosmArray)
+  componentValidation(s, 'unkown world', unkownWorldArray)
+  componentValidation(s, 'unkown node', unkownNodeArray)
+  componentValidation(s, 'data', dataArray)
+  componentValidation(s, 'actor', actorArray)
+  componentValidation(s, 'malicious actor', maliciousActorArray)
+  componentValidation(s, 'secure microcosm', secureMicrcosmArray)
+  componentValidation(s, 'asset', assetArray)
+  componentValidation(s, 'constraint', constraintArray)
+  componentValidation(s, 'property', propertyArray)
+  componentValidation(s, 'mechanism', mechanismArray)
+  componentValidation(s, 'threat', threatArray)
+  componentValidation(s, 'vulnerability', vulnerabilityArray)
 }
 
 let result = '' // posted on the nodeInfo div
-  // valid device connections
+let arrWrong = [] // stores wrong connection of nodes
+
+// valid component connections
 let deviceArray = ['secure microcosm', 'microcosm', 'vulnerability',
-  'network connection', 'data', 'actor'
+  'network connection', 'data', 'actor', 'asset'
 ]
-let networkArray = ['device', 'asset', 'data']
+let networkArray = ['device', 'actor', 'data']
+let microcosmArray = ['device', 'unkown world', 'vulnerability',
+  'secure microcosm']
+let unkownWorldArray = ['microcosm', 'unkown node', 'threat']
+let unkownNodeArray = ['unkown world', 'actor', 'malicious actor']
+let dataArray = ['asset', 'device', 'network connection', 'actor']
+let actorArray = ['asset', 'network connection', 'device', 'unkown node',
+  'data']
+let maliciousActorArray = actorArray.concat('threat')
+let secureMicrcosmArray = microcosmArray.concat('constraint')
+let assetArray = ['data', 'threat', 'actor', 'device']
+let constraintArray = ['secure microcosm', 'threat', 'property', 'mechanism']
+let propertyArray = ['constraint']
+let mechanismArray = ['constraint', 'vulnerability']
+let threatArray = ['asset', 'malicious actor', 'vulnerability', 'constraint',
+  'unkown world']
+let vulnerabilityArray = ['threat', 'microcosm', 'mechanism', 'device']
 
-function deviceValidation (s) {
-  let arrCorrect = [] // array with mitigated threats
-
+function componentValidation (s, component, componentArray) {
   for (let n of s.graph.nodes().values()) {
-    // checks if node is a device
-    if (n.info.type === 'device') {
-      // stores the neigborring nodes of the threats
+    // checks if node is the desired component
+    if (n.info.type === component) {
+      // stores the neighborring nodes of the component
       let neighborNodes = s.graph.neighbors(n.id)
 
       // to make stuff smaller
-      // every neighbor node is added to the array arrCorrect
+      // every neighbor node is added to the array arrWrong
       // if the neighbor is a valid connection it is removed from the array
       // in the end if the array in empty, the module is correct
       // if not, the values in the array will indicate the mistakes
       for (let i of Object.keys(neighborNodes)) {
-        arrCorrect.push(neighborNodes[i].info.type)
-        if (deviceArray.indexOf(neighborNodes[i].info.type) !== -1) {
-          arrCorrect.pop(neighborNodes[i].info.type)
+        arrWrong.push(neighborNodes[i].info.type)
+        if (componentArray.indexOf(neighborNodes[i].info.type) !== -1) {
+          arrWrong.pop(neighborNodes[i].info.type)
         }
-        result = `${arrCorrect}`
-      }
-    }
-    // checks if node is a network connection
-    if (n.info.type === 'network connection') {
-      // stores the neigborring nodes of the threats
-      let neighborNodes = s.graph.neighbors(n.id)
-
-      // to make stuff smaller
-      // every neighbor node is added to the array arrCorrect
-      // if the neighbor is a valid connection it is removed from the array
-      // in the end if the array in empty, the module is correct
-      // if not, the values in the array will indicate the mistakes
-      for (let i of Object.keys(neighborNodes)) {
-        arrCorrect.push(neighborNodes[i].info.type)
-        if (networkArray.indexOf(neighborNodes[i].info.type) !== -1) {
-          arrCorrect.pop(neighborNodes[i].info.type)
-        }
-        result = `${arrCorrect}`
+        result = `${arrWrong}`
         if (result === '') {
           result = 'module is valid'
         }
       }
     }
   }
+  // if (result === '') {
+  //   result = 'module is valid'
+  // }
   document.getElementById('infoForNodes').innerHTML = result
 }
