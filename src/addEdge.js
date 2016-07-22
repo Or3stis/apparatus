b6ae6e9fe88d7fbe8f07f3ac0993747a6f5f18e0
or3stis <or3stis.man@gmail.com>
2016-07-22 14:24:55 +0100

    now edges are fixed

--- src/addEdge.js
+++ src/addEdge.js
@@ -4,26 +4,32 @@
 module.exports = function addEdge (s, sourceNode, targetNode, lastEdge) {
   // finds the id of the last edge
   const addedEdge = lastEdge
+  let token = false
 
   s.graph.edges().map((e) => {
     // checks if the existing edge is a curved
     if (e.type !== 'curvedArrow') {
       if (sourceNode === e.source && targetNode === e.target) {
         document.getElementById('infoForNodes').innerHTML = 'edge exists'
+        token = true
         return
       } else if (sourceNode === e.target && targetNode === e.source) {
         document.getElementById('infoForNodes').innerHTML = 'edge exists'
+        token = true
         return
       } else {
+        token = false
         return
       }
-      s.graph.addEdge({
-        id: `e${addedEdge}`,
-        size: 0.1,
-        target: sourceNode,
-        source: targetNode
-      })
     }
   })
-  s.refresh()
+  if (token === false) {
+    s.graph.addEdge({
+      id: `e${addedEdge}`,
+      size: 0.1,
+      target: sourceNode,
+      source: targetNode
+    })
+    s.refresh()
+  }
 }
--- src/addOwnership.js
+++ src/addOwnership.js
@@ -3,20 +3,26 @@
 // shows info of node in the div 'infoForNodes'
 module.exports = function addEdge (s, sourceNode, targetNode, lastEdge) {
   const addedEdge = lastEdge
+  let token = false
 
   s.graph.edges().map((e) => {
     // checks if the existing edge is a curved
     if (e.type === 'curvedArrow') {
       if (sourceNode === e.source && targetNode === e.target) {
         document.getElementById('infoForNodes').innerHTML = 'edge exists'
+        token = true
         return
       } else if (sourceNode === e.target && targetNode === e.source) {
         document.getElementById('infoForNodes').innerHTML = 'edge exists'
+        token = true
         return
       } else {
+        token = false
         return
       }
     }
+  })
+  if (token === false) {
     s.graph.addEdge({
       id: `e${addedEdge}`,
       label: 'owns',
@@ -25,6 +31,6 @@ module.exports = function addEdge (s, sourceNode, targetNode, lastEdge) {
       target: sourceNode,
       source: targetNode
     })
-  })
-  s.refresh()
+    s.refresh()
+  }
 }
