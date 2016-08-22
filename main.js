e87c79f2623ef3ff2950639960b807b097dfa09d
or3stis <or3stis.man@gmail.com>
2016-08-23 00:05:53 +0100

    removed the title bar

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1200, height: 800})
+  mainWindow = new BrowserWindow({width: 1200, height: 800, titleBarStyle: 'hidden'})
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html')
