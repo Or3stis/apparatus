9bdf4524aa5916f115af4305e35ba9bf4d964b39
or3stis <or3stis.man@gmail.com>
2016-12-22 22:22:16 +0000

    fixed added body issue :tada:

--- main.js
+++ main.js
@@ -20,7 +20,8 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html')
+  // mainWindow.loadURL('file://' + __dirname + '/index.html')
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
