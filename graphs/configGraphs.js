14a4cbbb1214d81b617d38002f66d0ea5ca1ef6c
or3stis <or3stis.man@gmail.com>
2017-03-06 18:03:04 +0000

    updated color theme

--- graphs/configGraphs.js
+++ graphs/configGraphs.js
@@ -2,27 +2,22 @@
 
 const config = {}
 
-config.white = '#e5e9f0'
-config.darkWhite = '#eceff4'
+config.white = '#abb2bf'
+// config.darkWhite = '#eceff4'
 config.black = '#3b4251'
-config.deepBlack = '#2e3440'
-config.veryLightGray = '#bdc3c7'
-config.blue = '#61AFEF'
-// config.blue = '#82a2c0'
-config.red = '#bd626b'
+// config.deepBlack = '#2e3440'
+// config.veryLightGray = '#bdc3c7'
+config.blue = '#61afef'
+config.red = '#be5046'
 config.orange = '#cf8772'
-config.green = '#a4bd8e'
-config.yellow = '#eaca8f'
-config.gray = '#d8dee9'
+config.green = '#98c379'
+config.yellow = '#d19a66'
+config.magenta = '#c678dd'
+config.cyan = '#56b6c2'
+// config.gray = '#d8dee9'
 config.darkLine = '#242933'
 config.lightLine = '#e0e0e0'
-config.comment = '#d6d6d6'
+config.comment = '#abb2bf'
 // config.comment = '#94a4a5'
-
-// settings of sigma graph
-
-// values for the flagged list
-// see flagNodes.js
-config.flag = ['application', 'perception']
 
 module.exports = config
