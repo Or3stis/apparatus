88d056a3caa16b29945979181ddde4dd5b1c78dd
or3stis <or3stis.man@gmail.com>
2017-06-25 23:38:24 +0100

    :sparkles: cose layout on button

--- src/app.js
+++ src/app.js
@@ -151,7 +151,7 @@ if (window.location.pathname === dgnPath) {
     // reset selection selection
     // document.getElementById('module-group').selectedIndex = ''
   })
-// load design-state buttons
+  // load design-state buttons
 } else if (window.location.pathname === dgnStatePath) {
   // validate model
   const buttonModelValidate = document.getElementById('model-validate-button')
@@ -171,7 +171,7 @@ if (window.location.pathname === dgnPath) {
     document.getElementById('add-component-id').selectedIndex = ''
     totalNodes(cy) // global module
   })
-// loads implementation phase buttons
+  // loads implementation phase buttons
 } else if (window.location.pathname === impPath) {
   // validate model
   const buttonModelValidate = document.getElementById('model-validate-button')
@@ -218,7 +218,7 @@ if (window.location.pathname === dgnPath) {
     // reset selection selection
     // document.getElementById('module-group').selectedIndex = ''
   })
-// loads implementation-state buttons
+  // loads implementation-state buttons
 } else if (window.location.pathname === impStatePath) {
   // validate model
   const buttonModelValidate = document.getElementById('model-validate-button')
@@ -266,11 +266,63 @@ buttonNeighbor.addEventListener('click', () => {
   cy.elements().addClass('faded')
   neighborhood.removeClass('faded')
 })
-// random layout
+// cose layout, source: http://js.cytoscape.org/#layouts/cose
 const buttonLayout = document.getElementById('layout-button')
 buttonLayout.addEventListener('click', () => {
   const layout = cy.layout({
-    name: 'random'
+    name: 'cose',
+    // Called on `layoutready`
+    ready: function () {},
+    // Called on `layoutstop`
+    stop: function () {},
+    // Whether to animate while running the layout
+    animate: true,
+    // The layout animates only after this many milliseconds
+    // (prevents flashing on fast runs)
+    animationThreshold: 250,
+    // Number of iterations between consecutive screen positions update
+    // (0 -> only updated on the end)
+    refresh: 20,
+    // Whether to fit the network view after when done
+    fit: true,
+    // Padding on fit
+    padding: 30,
+    // Constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+    boundingBox: undefined,
+    // Excludes the label when calculating node bounding boxes for the layout algorithm
+    nodeDimensionsIncludeLabels: false,
+    // Randomize the initial positions of the nodes (true) or use existing positions (false)
+    randomize: false,
+    // Extra spacing between components in non-compound graphs
+    componentSpacing: 100,
+    // Node repulsion (non overlapping) multiplier
+    nodeRepulsion: function (node) {
+      return 400000
+    },
+    // Node repulsion (overlapping) multiplier
+    nodeOverlap: 10,
+    // Ideal edge (non nested) length
+    idealEdgeLength: function (edge) {
+      return 10
+    },
+    // Divisor to compute edge forces
+    edgeElasticity: function (edge) {
+      return 100
+    },
+    // Nesting factor (multiplier) to compute ideal edge length for nested edges
+    nestingFactor: 5,
+    // Gravity force (constant)
+    gravity: 80,
+    // Maximum number of iterations to perform
+    numIter: 1000,
+    // Initial temperature (maximum node displacement)
+    initialTemp: 200,
+    // Cooling factor (how the temperature is reduced between consecutive iterations
+    coolingFactor: 0.95,
+    // Lower temperature threshold (below this point the layout will end)
+    minTemp: 1.0,
+    // Pass a reference to weaver to use threads for calculations
+    weaver: false
   })
   layout.run()
 })
