70c45c5ce09f43178f4890119a739f0c25c6fab3
or3stis <or3stis.man@gmail.com>
2017-07-01 00:53:09 +0100

    :hammer: DRY

--- src/implementation-state/addImpStateEdge.js
+++ src/implementation-state/addImpStateEdge.js
@@ -2,147 +2,47 @@
 
 const printChat = require('../printChat.js')
 
+const addEdge = (cy, srcNode, trgNode, srcNodeCpt, trgNodeCp, label) => {
+  cy.add({
+    group: 'edges',
+    data: {
+      id: `e${srcNode}${trgNode}`,
+      source: `${srcNode}`,
+      target: `${trgNode}`,
+      label: `${label}`
+    }
+  })
+}
+
 module.exports = function addComponent (cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt) {
   if (srcNodeCpt === 'model' && trgNodeCpt === 'model') {
-    cy.add({
-      group: 'edges',
-      data: {
-        id: `e${srcNode}${trgNode}`,
-        source: `${srcNode}`,
-        target: `${trgNode}`,
-        label: 'configuration'
-      }
-    })
+    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'configuration')
   } else if (srcNodeCpt === 'event sensor' && trgNodeCpt === 'model') {
-    cy.add({
-      group: 'edges',
-      data: {
-        id: `e${srcNode}${trgNode}`,
-        source: `${srcNode}`,
-        target: `${trgNode}`,
-        label: 'belongs'
-      }
-    })
+    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'belongs')
   } else if (srcNodeCpt === 'event sensor' && trgNodeCpt === 'high-level event') {
-    cy.add({
-      group: 'edges',
-      data: {
-        id: `e${srcNode}${trgNode}`,
-        source: `${srcNode}`,
-        target: `${trgNode}`,
-        label: 'documents'
-      }
-    })
+    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'documents')
   } else if (srcNodeCpt === 'event sensor' && trgNodeCpt === 'low-level event') {
-    cy.add({
-      group: 'edges',
-      data: {
-        id: `e${srcNode}${trgNode}`,
-        source: `${srcNode}`,
-        target: `${trgNode}`,
-        label: 'documents'
-      }
-    })
+    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'documents')
   } else if (srcNodeCpt === 'report sensor' && trgNodeCpt === 'model') {
-    cy.add({
-      group: 'edges',
-      data: {
-        id: `e${srcNode}${trgNode}`,
-        source: `${srcNode}`,
-        target: `${trgNode}`,
-        label: 'belongs'
-      }
-    })
+    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'belongs')
   } else if (srcNodeCpt === 'report sensor' && trgNodeCpt === 'high-level event') {
-    cy.add({
-      group: 'edges',
-      data: {
-        id: `e${srcNode}${trgNode}`,
-        source: `${srcNode}`,
-        target: `${trgNode}`,
-        label: 'detects'
-      }
-    })
+    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'detects')
   } else if (srcNodeCpt === 'report sensor' && trgNodeCpt === 'low-level event') {
-    cy.add({
-      group: 'edges',
-      data: {
-        id: `e${srcNode}${trgNode}`,
-        source: `${srcNode}`,
-        target: `${trgNode}`,
-        label: 'detects'
-      }
-    })
+    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'detects')
   } else if (srcNodeCpt === 'report sensor' && trgNodeCpt === 'control sensor') {
-    cy.add({
-      group: 'edges',
-      data: {
-        id: `e${srcNode}${trgNode}`,
-        source: `${srcNode}`,
-        target: `${trgNode}`,
-        label: 'notifies'
-      }
-    })
+    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'notifies')
   } else if (srcNodeCpt === 'control sensor' && trgNodeCpt === 'model') {
-    cy.add({
-      group: 'edges',
-      data: {
-        id: `e${srcNode}${trgNode}`,
-        source: `${srcNode}`,
-        target: `${trgNode}`,
-        label: 'belongs'
-      }
-    })
+    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'belongs')
   } else if (srcNodeCpt === 'control sensor' && trgNodeCpt === 'event sensor') {
-    cy.add({
-      group: 'edges',
-      data: {
-        id: `e${srcNode}${trgNode}`,
-        source: `${srcNode}`,
-        target: `${trgNode}`,
-        label: 'listens'
-      }
-    })
+    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'listens')
   } else if (srcNodeCpt === 'control sensor' && trgNodeCpt === 'high-level event') {
-    cy.add({
-      group: 'edges',
-      data: {
-        id: `e${srcNode}${trgNode}`,
-        source: `${srcNode}`,
-        target: `${trgNode}`,
-        label: 'detects'
-      }
-    })
+    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'detects')
   } else if (srcNodeCpt === 'control sensor' && trgNodeCpt === 'low-level event') {
-    cy.add({
-      group: 'edges',
-      data: {
-        id: `e${srcNode}${trgNode}`,
-        source: `${srcNode}`,
-        target: `${trgNode}`,
-        label: 'detects'
-      }
-    })
+    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'detects')
   } else if (srcNodeCpt === 'high-level event' && trgNodeCpt === 'model') {
-    cy.add({
-      group: 'edges',
-      data: {
-        id: `e${srcNode}${trgNode}`,
-        source: `${srcNode}`,
-        target: `${trgNode}`,
-        label: 'affects'
-      }
-    })
+    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'affects')
   } else if (srcNodeCpt === 'low-level event' && trgNodeCpt === 'model') {
-    cy.add({
-      group: 'edges',
-      data: {
-        id: `e${srcNode}${trgNode}`,
-        source: `${srcNode}`,
-        target: `${trgNode}`,
-        label: 'affects'
-      }
-    })
+    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'affects')
   } else {
     printChat('mistake 😔')
   }
