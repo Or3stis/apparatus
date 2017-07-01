bd53d44bf030e119edea24676e23d8de4f099a3d
or3stis <or3stis.man@gmail.com>
2017-07-01 11:26:20 +0100

    :hammer: -> to switch statement

--- src/implementation/addImpEdge.js
+++ src/implementation/addImpEdge.js
@@ -4,89 +4,131 @@ const printChat = require('../printChat.js')
 const addEdge = require('../addEdge.js')
 
 module.exports = function addComponent (cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt) {
-  if (srcNodeCpt === 'micronet' && trgNodeCpt === 'micronet') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'connects')
-  } else if (srcNodeCpt === 'device' && trgNodeCpt === 'device') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'composed')
-  } else if (srcNodeCpt === 'device' && trgNodeCpt === 'micronet') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'belongs')
-  } else if (srcNodeCpt === 'device' && trgNodeCpt === 'information') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'has')
-  } else if (srcNodeCpt === 'net' && trgNodeCpt === 'micronet') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'requests')
-  } else if (srcNodeCpt === 'net' && trgNodeCpt === 'threat') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'poses')
-  } else if (srcNodeCpt === 'unidentified node' && trgNodeCpt === 'net') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'belongs')
-  } else if (srcNodeCpt === 'network connection' && trgNodeCpt === 'device') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'connects')
-  } else if (srcNodeCpt === 'network connection' && trgNodeCpt === 'information') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'has')
-  } else if (srcNodeCpt === 'information' && trgNodeCpt === 'device') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'requires')
-  } else if (srcNodeCpt === 'information' && trgNodeCpt === 'application') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'requires')
-  } else if (srcNodeCpt === 'application' && trgNodeCpt === 'device') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'runs')
-  } else if (srcNodeCpt === 'application' && trgNodeCpt === 'information') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'has')
-  } else if (srcNodeCpt === 'actor' && trgNodeCpt === 'application') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'uses')
-  } else if (srcNodeCpt === 'actor' && trgNodeCpt === 'unidentified node') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'uses')
-  } else if (srcNodeCpt === 'actor' && trgNodeCpt === 'device') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'uses')
-  } else if (srcNodeCpt === 'actor' && trgNodeCpt === 'information') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'knows')
-  } else if (srcNodeCpt === 'malicious actor' && trgNodeCpt === 'application') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'uses')
-  } else if (srcNodeCpt === 'malicious actor' && trgNodeCpt === 'unidentified node') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'uses')
-  } else if (srcNodeCpt === 'malicious actor' && trgNodeCpt === 'device') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'uses')
-  } else if (srcNodeCpt === 'malicious actor' && trgNodeCpt === 'information') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'knows')
-  } else if (srcNodeCpt === 'malicious actor' && trgNodeCpt === 'threat') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'poses')
-  } else if (srcNodeCpt === 'application' && trgNodeCpt === 'report sensor') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'report sensor')
-  } else if (srcNodeCpt === 'application' && trgNodeCpt === 'contorl sensor') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'is')
-  } else if (srcNodeCpt === 'application' && trgNodeCpt === 'event sensor') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'is')
-  } else if (srcNodeCpt === 'report sensor' && trgNodeCpt === 'control sensor') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'notifies')
-  } else if (srcNodeCpt === 'control sensor' && trgNodeCpt === 'event sensor') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'listens')
-  } else if (srcNodeCpt === 'control sensor' && trgNodeCpt === 'mechanism') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'triggers')
-  } else if (srcNodeCpt === 'constraint' && trgNodeCpt === 'threat') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'mitigates')
-  } else if (srcNodeCpt === 'constraint' && trgNodeCpt === 'micronet') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'imposes')
-  } else if (srcNodeCpt === 'mechanism' && trgNodeCpt === 'constraint') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'satisfies')
-  } else if (srcNodeCpt === 'mechanism' && trgNodeCpt === 'vulnerability') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'protects')
-  } else if (srcNodeCpt === 'threat' && trgNodeCpt === 'vulnerability') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'exploits')
-  } else if (srcNodeCpt === 'threat' && trgNodeCpt === 'asset') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'targets')
-  } else if (srcNodeCpt === 'vulnerability' && trgNodeCpt === 'micronet') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'affects')
-  } else if (srcNodeCpt === 'vulnerability' && trgNodeCpt === 'device') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'affects')
-  } else if (srcNodeCpt === 'vulnerability' && trgNodeCpt === 'network connection') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'affects')
-  } else if (srcNodeCpt === 'vulnerability' && trgNodeCpt === 'application') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'affects')
-  } else if (srcNodeCpt === 'asset' && trgNodeCpt === 'device') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'is')
-  } else if (srcNodeCpt === 'asset' && trgNodeCpt === 'application') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'is')
-  } else if (srcNodeCpt === 'asset' && trgNodeCpt === 'information') {
-    addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'is')
-  } else {
-    printChat('mistake 😔')
+  switch (true) {
+    case (srcNodeCpt === 'micronet' && trgNodeCpt === 'micronet'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'connects')
+      break
+    case (srcNodeCpt === 'device' && trgNodeCpt === 'device'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'composed')
+      break
+    case (srcNodeCpt === 'device' && trgNodeCpt === 'micronet'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'belongs')
+      break
+    case (srcNodeCpt === 'device' && trgNodeCpt === 'information'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'has')
+      break
+    case (srcNodeCpt === 'net' && trgNodeCpt === 'micronet'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'requests')
+      break
+    case (srcNodeCpt === 'net' && trgNodeCpt === 'threat'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'poses')
+      break
+    case (srcNodeCpt === 'unidentified node' && trgNodeCpt === 'net'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'belongs')
+      break
+    case (srcNodeCpt === 'network connection' && trgNodeCpt === 'device'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'connects')
+      break
+    case (srcNodeCpt === 'network connection' && trgNodeCpt === 'information'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'has')
+      break
+    case (srcNodeCpt === 'information' && trgNodeCpt === 'device'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'requires')
+      break
+    case (srcNodeCpt === 'information' && trgNodeCpt === 'application'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'requires')
+      break
+    case (srcNodeCpt === 'application' && trgNodeCpt === 'device'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'runs')
+      break
+    case (srcNodeCpt === 'application' && trgNodeCpt === 'information'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'has')
+      break
+    case (srcNodeCpt === 'actor' && trgNodeCpt === 'application'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'uses')
+      break
+    case (srcNodeCpt === 'actor' && trgNodeCpt === 'unidentified node'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'uses')
+      break
+    case (srcNodeCpt === 'actor' && trgNodeCpt === 'device'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'uses')
+      break
+    case (srcNodeCpt === 'actor' && trgNodeCpt === 'information'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'knows')
+      break
+    case (srcNodeCpt === 'malicious actor' && trgNodeCpt === 'application'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'uses')
+      break
+    case (srcNodeCpt === 'malicious actor' && trgNodeCpt === 'unidentified node'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'uses')
+      break
+    case (srcNodeCpt === 'malicious actor' && trgNodeCpt === 'device'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'uses')
+      break
+    case (srcNodeCpt === 'malicious actor' && trgNodeCpt === 'information'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'knows')
+      break
+    case (srcNodeCpt === 'malicious actor' && trgNodeCpt === 'threat'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'poses')
+      break
+    case (srcNodeCpt === 'application' && trgNodeCpt === 'report sensor'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'report sensor')
+      break
+    case (srcNodeCpt === 'application' && trgNodeCpt === 'contorl sensor'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'is')
+      break
+    case (srcNodeCpt === 'application' && trgNodeCpt === 'event sensor'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'is')
+      break
+    case (srcNodeCpt === 'report sensor' && trgNodeCpt === 'control sensor'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'notifies')
+      break
+    case (srcNodeCpt === 'control sensor' && trgNodeCpt === 'event sensor'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'listens')
+      break
+    case (srcNodeCpt === 'control sensor' && trgNodeCpt === 'mechanism'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'triggers')
+      break
+    case (srcNodeCpt === 'constraint' && trgNodeCpt === 'threat'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'mitigates')
+      break
+    case (srcNodeCpt === 'constraint' && trgNodeCpt === 'micronet'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'imposes')
+      break
+    case (srcNodeCpt === 'mechanism' && trgNodeCpt === 'constraint'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'satisfies')
+      break
+    case (srcNodeCpt === 'mechanism' && trgNodeCpt === 'vulnerability'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'protects')
+      break
+    case (srcNodeCpt === 'threat' && trgNodeCpt === 'vulnerability'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'exploits')
+      break
+    case (srcNodeCpt === 'threat' && trgNodeCpt === 'asset'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'targets')
+      break
+    case (srcNodeCpt === 'vulnerability' && trgNodeCpt === 'micronet'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'affects')
+      break
+    case (srcNodeCpt === 'vulnerability' && trgNodeCpt === 'device'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'affects')
+      break
+    case (srcNodeCpt === 'vulnerability' && trgNodeCpt === 'network connection'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'affects')
+      break
+    case (srcNodeCpt === 'vulnerability' && trgNodeCpt === 'application'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'affects')
+      break
+    case (srcNodeCpt === 'asset' && trgNodeCpt === 'device'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'is')
+      break
+    case (srcNodeCpt === 'asset' && trgNodeCpt === 'application'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'is')
+      break
+    case (srcNodeCpt === 'asset' && trgNodeCpt === 'information'):
+      addEdge(cy, srcNode, trgNode, srcNodeCpt, trgNodeCpt, 'is')
+      break
+    default:
+      printChat('edge not allowed 😔')
   }
 }
