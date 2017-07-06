38aed038b4aaebbe055be46513c6fd1c4d977ae7
or3stis <or3stis.man@gmail.com>
2017-07-06 17:40:22 +0100

    push selections as arguments

--- src/keybindings.js
+++ src/keybindings.js
@@ -5,40 +5,22 @@ const printChat = require('./core/printChat.js')
 const totalNodes = require('./core/totalNodes.js')
 // const addEdge = require('./addEdge.js')
 
-module.exports = function keyboard (cy, toggleUI) {
+module.exports = function keyboard (cy, selectedNode, selectedEdge, toggleUI) {
   // help menu
   const helpMenu =
     '• help: for options\n• backspace: delete node/edge\n• meta + z: to undo last action\n• alt + h: toggle UI\n• meta + l: focus on console\n• search for attributes\n'
 
   const consoleId = document.getElementById('console-id')
 
-  let selectedNode = ''
-  let selectedEdge = ''
-  // let sourceNode = ''
-  // let targetNode = ''
-
+  // loses the focus from the console when tapping
   cy.on('tap', 'node', selection => {
-    selectedNode = selection.target[0]
-    selectedEdge = ''
-    // sourceNode = targetNode // second selection
-    // targetNode = selectedNode.data().id
-    // loses the focus from the console when tapping a node
     consoleId.blur()
   })
   cy.on('tap', 'edge', selection => {
-    selectedEdge = selection.target[0]
-    selectedNode = ''
-    // loses the focus from the console when tapping an edge
     consoleId.blur()
   })
-  // empties the selection values when clicking the graph
   cy.on('tap', selection => {
-    // loses the focus from the console when tapping the stage
     consoleId.blur()
-    if (selection.target === cy) {
-      selectedEdge = ''
-      selectedNode = ''
-    }
   })
 
   // keyboard listeners
@@ -53,11 +35,17 @@ module.exports = function keyboard (cy, toggleUI) {
     }
     // Backspace deletion of nodes and edges
     if (document.activeElement !== consoleId && event.code === 'Backspace') {
-      if (selectedNode === '' && selectedEdge !== '') {
-        selectedEdge.remove()
+      if (
+        Object.keys(selectedNode.out).length === 0 &&
+        Object.keys(selectedEdge.out).length !== 0
+      ) {
+        selectedEdge.out.remove()
       }
-      if (selectedEdge === '' && selectedNode !== '') {
-        selectedNode.remove()
+      if (
+        Object.keys(selectedEdge.out).length === 0 &&
+        Object.keys(selectedNode.out).length !== 0
+      ) {
+        selectedNode.out.remove()
       }
       totalNodes(cy) // global module
     }
@@ -70,7 +58,6 @@ module.exports = function keyboard (cy, toggleUI) {
     if (event.metaKey === true && event.code === 'KeyZ') {
       selectedNode.restore()
     }
-
     // console commands
     const input = document.getElementById('console-id').value
     // listens for the ENTER key
