01dc1c8374f6c9108ddfac1ccb21c05b431cf303
or3stis <or3stis.man@gmail.com>
2017-07-16 22:02:44 +0100

    :sparkles: cyOptions module

---
+++ src/core/cyOptions.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// not used
+
+const cytoscape = require('cytoscape')
+const graphStyle = require('../../config/graphStyle.js')
+
+module.exports = function cyOptions (cy, file) {
+  let model = require(file)
+  cy.out = cytoscape({
+    container: document.getElementById('graph-container'),
+    autounselectify: true,
+    elements: model.elements, // loads the elements object of the graph
+    style: graphStyle.style
+  })
+  // graph layout
+  cy.out.layout({
+    name: 'cose'
+  })
+}
