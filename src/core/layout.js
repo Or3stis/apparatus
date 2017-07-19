1d2fe29f6463b9c0ba1e92b09250b701ef6b9a7c
or3stis <or3stis.man@gmail.com>
2017-07-19 22:33:31 +0100

    :sparkles: grid layout

--- src/core/layout.js
+++ src/core/layout.js
@@ -96,6 +96,26 @@ module.exports = function layout (cy, selection) {
     ready: undefined, // callback on layoutready
     stop: undefined // callback on layoutstop
   })
+  const grid = cy.layout({
+    name: 'grid',
+    fit: true, // whether to fit the viewport to the graph
+    padding: 30, // padding used on fit
+    boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+    avoidOverlap: true, // prevents node overlap, may overflow boundingBox if not enough space
+    avoidOverlapPadding: 10, // extra spacing around nodes when avoidOverlap: true
+    nodeDimensionsIncludeLabels: false, // Excludes the label when calculating node bounding boxes for the layout algorithm
+    spacingFactor: undefined, // Applies a multiplicative factor (>0) to expand or compress the overall area that the nodes take up
+    condense: false, // uses all available space on false, uses minimal space on true
+    rows: undefined, // force num of rows in the grid
+    cols: undefined, // force num of columns in the grid
+    position: function (node) {}, // returns { row, col } for element
+    sort: undefined, // a sorting function to order the nodes; e.g. function(a, b){ return a.data('weight') - b.data('weight') }
+    animate: false, // whether to transition the node positions
+    animationDuration: 500, // duration of animation in ms if enabled
+    animationEasing: undefined, // easing of animation if enabled
+    ready: undefined, // callback on layoutready
+    stop: undefined // callback on layoutstop
+  })
 
   switch (selection) {
     case 'cose':
@@ -106,6 +126,9 @@ module.exports = function layout (cy, selection) {
       break
     case 'circle':
       circle.run()
+      break
+    case 'grid':
+      grid.run()
       break
     default:
       console.error('error in layout.js')
