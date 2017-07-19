65cc78c347c41deb6d7cd8531c39d616cee696c7
or3stis <or3stis.man@gmail.com>
2017-07-19 22:40:55 +0100

    :art:

--- src/core/layout.js
+++ src/core/layout.js
@@ -7,20 +7,18 @@ module.exports = function layout (cy, selection) {
   const coseLayout = cy.layout({
     name: 'cose',
     // Called on `layoutready`
-    ready: function () {},
+    ready: () => {},
     // Called on `layoutstop`
-    stop: function () {},
+    stop: () => {},
     // Whether to animate while running the layout
-    animate: true,
+    animate: false,
     // The layout animates only after this many milliseconds
     // (prevents flashing on fast runs)
     animationThreshold: 250,
     // Number of iterations between consecutive screen positions update
     // (0 -> only updated on the end)
     refresh: 20,
-    // Whether to fit the network view after when done
     fit: true,
-    // Padding on fit
     padding: 30,
     // Constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
     boundingBox: undefined,
@@ -31,17 +29,17 @@ module.exports = function layout (cy, selection) {
     // Extra spacing between components in non-compound graphs
     componentSpacing: 100,
     // Node repulsion (non overlapping) multiplier
-    nodeRepulsion: function (node) {
+    nodeRepulsion: (node) => {
       return 400000
     },
     // Node repulsion (overlapping) multiplier
     nodeOverlap: 10,
     // Ideal edge (non nested) length
-    idealEdgeLength: function (edge) {
+    idealEdgeLength: (edge) => {
       return 10
     },
     // Divisor to compute edge forces
-    edgeElasticity: function (edge) {
+    edgeElasticity: (edge) => {
       return 100
     },
     // Nesting factor (multiplier) to compute ideal edge length for nested edges
@@ -61,9 +59,9 @@ module.exports = function layout (cy, selection) {
   })
   const breadLayout = cy.layout({
     name: 'breadthfirst',
-    fit: true, // whether to fit the viewport to the graph
+    fit: true,
     directed: false, // whether the tree is directed downwards (or edges can point in any direction if false)
-    padding: 30, // padding on fit
+    padding: 30,
     circle: false, // put depths in concentric circles if true, put depths top down if false
     spacingFactor: 1.75, // positive spacing factor, larger => more space between nodes (N.B. n/a if causes overlap)
     boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
@@ -79,8 +77,8 @@ module.exports = function layout (cy, selection) {
   })
   const circle = cy.layout({
     name: 'circle',
-    fit: true, // whether to fit the viewport to the graph
-    padding: 30, // the padding on fit
+    fit: true,
+    padding: 30,
     boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
     avoidOverlap: true, // prevents node overlap, may overflow boundingBox and radius if not enough space
     nodeDimensionsIncludeLabels: false, // Excludes the label when calculating node bounding boxes for the layout algorithm
@@ -90,7 +88,7 @@ module.exports = function layout (cy, selection) {
     sweep: undefined, // how many radians should be between the first and last node (defaults to full circle)
     clockwise: true, // whether the layout should go clockwise (true) or counterclockwise/anticlockwise (false)
     sort: undefined, // a sorting function to order the nodes; e.g. function(a, b){ return a.data('weight') - b.data('weight') }
-    animate: false, // whether to transition the node positions
+    animate: false,
     animationDuration: 500, // duration of animation in ms if enabled
     animationEasing: undefined, // easing of animation if enabled
     ready: undefined, // callback on layoutready
@@ -98,8 +96,8 @@ module.exports = function layout (cy, selection) {
   })
   const grid = cy.layout({
     name: 'grid',
-    fit: true, // whether to fit the viewport to the graph
-    padding: 30, // padding used on fit
+    fit: true,
+    padding: 30,
     boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
     avoidOverlap: true, // prevents node overlap, may overflow boundingBox if not enough space
     avoidOverlapPadding: 10, // extra spacing around nodes when avoidOverlap: true
@@ -108,7 +106,7 @@ module.exports = function layout (cy, selection) {
     condense: false, // uses all available space on false, uses minimal space on true
     rows: undefined, // force num of rows in the grid
     cols: undefined, // force num of columns in the grid
-    position: function (node) {}, // returns { row, col } for element
+    position: (node) => {}, // returns { row, col } for element
     sort: undefined, // a sorting function to order the nodes; e.g. function(a, b){ return a.data('weight') - b.data('weight') }
     animate: false, // whether to transition the node positions
     animationDuration: 500, // duration of animation in ms if enabled
