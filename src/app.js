b201aca63233595122165d43bfda3e82db62c68c
or3stis <or3stis.man@gmail.com>
2017-07-19 20:56:13 +0100

    comment debug section

--- src/app.js
+++ src/app.js
@@ -1,77 +1,77 @@
 'use strict'
 
-// const load = require('./src/core/load.js')
-// const initialize = require('./src/initialize.js')
-// const cyOptions = require('./src/core/cyOptions.js')
-//
-// const template = '../../graphs/modelTemplate.js'
-// const testGraph = '../../graphs/implementation/smartHome.js'
-//
-// let cy = {}
-//
-// // loads the empty template
-// const newGraph = () => {
-//   cyOptions(cy, template)
-//   initialize(cy.out)
-// }
-// // loads the debugging graph
-// const debugGraph = () => {
-//   cyOptions(cy, testGraph)
-//   initialize(cy.out)
-// }
-//
-// // TODO move it to different modules
-// // creates the option menu on startup
-// // the element is removed on selection
-// const start = () => {
-//   const graph = document.getElementById('graph-container')
-//   const wrapper = document.createElement('wrapper')
-//   wrapper.id = 'wrapper-id'
-//   wrapper.className = 'wrapper'
-//   wrapper.textContent = 'select'
-//
-//   const buttonNew = document.createElement('button')
-//   buttonNew.id = 'new-id'
-//   buttonNew.className = 'startButtons'
-//   buttonNew.type = 'button'
-//   buttonNew.value = 'new'
-//   buttonNew.textContent = 'new graph'
-//
-//   const buttonLoad = document.createElement('button')
-//   buttonLoad.id = 'load-id'
-//   buttonLoad.className = 'startButtons'
-//   buttonLoad.type = 'button'
-//   buttonLoad.value = 'load'
-//   buttonLoad.textContent = 'load graph'
-//
-//   const buttonDebug = document.createElement('button')
-//   buttonDebug.id = 'debug-id'
-//   buttonDebug.className = 'startButtons'
-//   buttonDebug.type = 'button'
-//   buttonDebug.value = 'debug'
-//   buttonDebug.textContent = 'bebug app'
-//
-//   wrapper.appendChild(buttonNew)
-//   wrapper.appendChild(buttonLoad)
-//   wrapper.appendChild(buttonDebug)
-//
-//   graph.appendChild(wrapper)
-//
-//   buttonNew.addEventListener('click', () => {
-//     newGraph()
-//     graph.removeChild(wrapper)
-//   })
-//   buttonLoad.addEventListener('click', () => {
-//     load(cy)
-//     graph.removeChild(wrapper)
-//   })
-//   buttonDebug.addEventListener('click', () => {
-//     debugGraph(cy)
-//     graph.removeChild(wrapper)
-//   })
-// }
-//
-// start()
+const load = require('./src/core/load.js')
+const initialize = require('./src/initialize.js')
+const cyOptions = require('./src/core/cyOptions.js')
+
+const template = '../../graphs/modelTemplate.js'
+const testGraph = '../../graphs/implementation/smartHome.js'
+
+let cy = {}
+
+// loads the empty template
+const newGraph = () => {
+  cyOptions(cy, template)
+  initialize(cy.out)
+}
+// loads the debugging graph
+const debugGraph = () => {
+  cyOptions(cy, testGraph)
+  initialize(cy.out)
+}
+
+// TODO move it to different modules
+// creates the option menu on startup
+// the element is removed on selection
+const start = () => {
+  const graph = document.getElementById('graph-container')
+  const wrapper = document.createElement('wrapper')
+  wrapper.id = 'wrapper-id'
+  wrapper.className = 'wrapper'
+  wrapper.textContent = 'select'
+
+  const buttonNew = document.createElement('button')
+  buttonNew.id = 'new-id'
+  buttonNew.className = 'startButtons'
+  buttonNew.type = 'button'
+  buttonNew.value = 'new'
+  buttonNew.textContent = 'new graph'
+
+  const buttonLoad = document.createElement('button')
+  buttonLoad.id = 'load-id'
+  buttonLoad.className = 'startButtons'
+  buttonLoad.type = 'button'
+  buttonLoad.value = 'load'
+  buttonLoad.textContent = 'load graph'
+
+  const buttonDebug = document.createElement('button')
+  buttonDebug.id = 'debug-id'
+  buttonDebug.className = 'startButtons'
+  buttonDebug.type = 'button'
+  buttonDebug.value = 'debug'
+  buttonDebug.textContent = 'bebug app'
+
+  wrapper.appendChild(buttonNew)
+  wrapper.appendChild(buttonLoad)
+  wrapper.appendChild(buttonDebug)
+
+  graph.appendChild(wrapper)
+
+  buttonNew.addEventListener('click', () => {
+    newGraph()
+    graph.removeChild(wrapper)
+  })
+  buttonLoad.addEventListener('click', () => {
+    load(cy)
+    graph.removeChild(wrapper)
+  })
+  buttonDebug.addEventListener('click', () => {
+    debugGraph(cy)
+    graph.removeChild(wrapper)
+  })
+}
+
+start()
 
 /*
 The default behavior of the app (asking to load graphs) can be annoying
@@ -82,10 +82,10 @@ the app.
 
 // uncomment the code below for debugging
 
-const testGraph = '../../graphs/implementation/smartHome.js'
-const initialize = require('./src/initialize.js')
-const cyOptions = require('./src/core/cyOptions.js')
-
-let cy = {}
-cyOptions(cy, testGraph) // uncomment for debugging
-initialize(cy.out) // uncomment for debugging
+// const testGraph = '../../graphs/implementation/smartHome.js'
+// const initialize = require('./src/initialize.js')
+// const cyOptions = require('./src/core/cyOptions.js')
+//
+// let cy = {}
+// cyOptions(cy, testGraph) // uncomment for debugging
+// initialize(cy.out) // uncomment for debugging
