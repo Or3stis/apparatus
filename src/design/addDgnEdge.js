18cdae7a5cfb4c761a1fb4fc0605cb0c98050890
or3stis <or3stis.man@gmail.com>
2017-08-06 22:33:38 +0100

    :fire: remove legacy addEdge.js

--- src/design/addDgnEdge.js
+++ src/design/addDgnEdge.js
@@ -11,58 +11,58 @@ module.exports = function addComponent (cy, srcNode, trgNode) {
 
   switch (true) {
     case srcNodeCpt === 'net' && trgNodeCpt === 'micronet':
-      addEdge(cy, srcNodeId, trgNodeId, srcNodeCpt, trgNodeCpt, 'requests')
+      addEdge(cy, srcNodeId, trgNodeId, 'requests')
       break
     case srcNodeCpt === 'thing' && trgNodeCpt === 'micronet':
-      addEdge(cy, srcNodeId, trgNodeId, srcNodeCpt, trgNodeCpt, 'belongs')
+      addEdge(cy, srcNodeId, trgNodeId, 'belongs')
       break
     case srcNodeCpt === 'thing' && trgNodeCpt === 'thing':
-      addEdge(cy, srcNodeId, trgNodeId, srcNodeCpt, trgNodeCpt, 'connects')
+      addEdge(cy, srcNodeId, trgNodeId, 'connects')
       break
     case srcNodeCpt === 'thing' && trgNodeCpt === 'information':
       addEdge(cy, srcNodeId, trgNodeId, srcNodeCpt, trgNodeCpt, 'has')
       break
     case srcNodeCpt === 'constraint' && trgNodeCpt === 'micronet':
-      addEdge(cy, srcNodeId, trgNodeId, srcNodeCpt, trgNodeCpt, 'imposes')
+      addEdge(cy, srcNodeId, trgNodeId, 'imposes')
       break
     case srcNodeCpt === 'threat' && trgNodeCpt === 'thing':
-      addEdge(cy, srcNodeId, trgNodeId, srcNodeCpt, trgNodeCpt, 'poses')
+      addEdge(cy, srcNodeId, trgNodeId, 'poses')
       break
     case srcNodeCpt === 'asset' && trgNodeCpt === 'thing':
-      addEdge(cy, srcNodeId, trgNodeId, srcNodeCpt, trgNodeCpt, 'is')
+      addEdge(cy, srcNodeId, trgNodeId, 'is')
       break
     case srcNodeCpt === 'asset' && trgNodeCpt === 'information':
-      addEdge(cy, srcNodeId, trgNodeId, srcNodeCpt, trgNodeCpt, 'is')
+      addEdge(cy, srcNodeId, trgNodeId, 'is')
       break
     case srcNodeCpt === 'information' && trgNodeCpt === 'thing':
-      addEdge(cy, srcNodeId, trgNodeId, srcNodeCpt, trgNodeCpt, 'requires')
+      addEdge(cy, srcNodeId, trgNodeId, 'requires')
       break
     case srcNodeCpt === 'actor' && trgNodeCpt === 'thing':
-      addEdge(cy, srcNodeId, trgNodeId, srcNodeCpt, trgNodeCpt, 'use')
+      addEdge(cy, srcNodeId, trgNodeId, 'use')
       break
     case srcNodeCpt === 'actor' && trgNodeCpt === 'information':
       addEdge(cy, srcNodeId, trgNodeId, srcNodeCpt, trgNodeCpt, 'knows')
       break
     case srcNodeCpt === 'thing' && trgNodeCpt === 'net':
-      addEdge(cy, srcNodeId, trgNodeId, srcNodeCpt, trgNodeCpt, 'belongs')
+      addEdge(cy, srcNodeId, trgNodeId, 'belongs')
       break
     case srcNodeCpt === 'threat' && trgNodeCpt === 'asset':
-      addEdge(cy, srcNodeId, trgNodeId, srcNodeCpt, trgNodeCpt, 'targets')
+      addEdge(cy, srcNodeId, trgNodeId, 'targets')
       break
     case srcNodeCpt === 'malicious actor' && trgNodeCpt === 'threat':
-      addEdge(cy, srcNodeId, trgNodeId, srcNodeCpt, trgNodeCpt, 'poses')
+      addEdge(cy, srcNodeId, trgNodeId, 'poses')
       break
     case srcNodeCpt === 'malicious actor' && trgNodeCpt === 'thing':
-      addEdge(cy, srcNodeId, trgNodeId, srcNodeCpt, trgNodeCpt, 'uses')
+      addEdge(cy, srcNodeId, trgNodeId, 'uses')
       break
     case srcNodeCpt === 'malicious actor' && trgNodeCpt === 'information':
-      addEdge(cy, srcNodeId, trgNodeId, srcNodeCpt, trgNodeCpt, 'uses')
+      addEdge(cy, srcNodeId, trgNodeId, 'uses')
       break
     case srcNodeCpt === 'constraint' && trgNodeCpt === 'threat':
-      addEdge(cy, srcNodeId, trgNodeId, srcNodeCpt, trgNodeCpt, 'mitigates')
+      addEdge(cy, srcNodeId, trgNodeId, 'mitigates')
       break
     case srcNodeCpt === 'micronet' && trgNodeCpt === 'micronet':
-      addEdge(cy, srcNodeId, trgNodeId, srcNodeCpt, trgNodeCpt, 'connects')
+      addEdge(cy, srcNodeId, trgNodeId, 'connects')
       break
     default:
       printChat(`${srcNodeCpt} → ${trgNodeCpt}\nnot allowed 😔`)
