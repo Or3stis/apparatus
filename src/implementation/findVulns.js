b03cce35bd317b9f695a0a588fbbd6dc22f3a5e2
or3stis <or3stis.man@gmail.com>
2017-08-13 22:02:18 +0100

    store found vulnerabilities to analysis folder

--- src/implementation/findVulns.js
+++ src/implementation/findVulns.js
@@ -1,7 +1,9 @@
 // finds vulnerabilities from CVE database
 
 const printChat = require('../helpers/printChat.js')
+const printChatHTML = require('../helpers/printChatHTML.js')
 const http = require('http')
+const fs = require('fs')
 
 // only checks for the concepts of device and application
 module.exports = function findVuln (cy) {
@@ -16,24 +18,35 @@ module.exports = function findVuln (cy) {
     if (node.data().info.concept === 'device') {
       keywords += `• ${node.data().info.type}\n`
       vulnInfo[node.data().id] = node.data().info.type
+
       node.removeClass('faded')
       node.addClass('attention')
     } else if (node.data().info.concept === 'application') {
       keywords += `• ${node.data().info.version}\n`
       vulnInfo[node.data().id] = node.data().info.version
+
       node.removeClass('faded')
       node.addClass('attention')
     }
   })
+
   if (keywords === '') {
-    keywords = 'no vulnerabilities were found'
+    printChat('no vulnerabilities were found')
+  } else {
+    printChat('sending request to http://cve/circ.lu')
+    printChat(`☛ keywords used:\n\n${keywords}`)
+    requestVulnData(vulnInfo)
   }
-  printChat(keywords)
-  // requestVulnData(vulnInfo)
 }
 
 // request vulnerability information from cve
 const requestVulnData = vulnInfo => {
+  // get the name of the fileNames
+  const title = document.getElementById('title-bar-id')
+  const file = title.textContent.split('/').pop()
+
+  // stores the CVE list
+  let totalVuln = []
   Object.keys(vulnInfo).map(vuln => {
     http
       .get(`http://cve.circl.lu/api/search/${vulnInfo[vuln]}`, resp => {
@@ -46,16 +59,34 @@ const requestVulnData = vulnInfo => {
 
         // The whole response has been received. Print out the result.
         resp.on('end', () => {
-          // console.log(JSON.parse(data))
+          // write the results
+
+          fs.writeFile(`analysis/vulnerability-${file}`, data, err => {
+            if (err) throw err
+          })
+          // this works for office
           Object.values(JSON.parse(data)).map(key => {
-            key.map((i) => {
-              console.log(`${vuln} ${i.id}`)
+            key.map(info => {
+              // CVE id
+              totalVuln.push(info.id)
             })
           })
+          // this works for microsoft/office
+          // JSON.parse(data).map((key) => {
+          // console.log(key)
+          // })
+
+          // displays the total amount of vulnerabilities
+          printChat(
+            `${vulnInfo[vuln]} vulnerabilities found: ${totalVuln.length}`
+          )
         })
       })
       .on('error', err => {
-        console.log('Error: ' + err.message)
+        console.log(`Error: ${err.message}`)
       })
   })
+  printChatHTML(
+    `analysis results will be stored at\n<strong>./analysis/vulnerability-${file}</strong>`
+  )
 }
