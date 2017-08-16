b4970b7bfbe23ad45129455628d6cface5cb8ebd
or3stis <or3stis.man@gmail.com>
2017-08-16 17:27:38 +0100

    :art: code cleanup

--- main.js
+++ main.js
@@ -44,15 +44,11 @@ app.on('ready', createWindow)
 app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+  if (mainWindow === null) createWindow()
 })
