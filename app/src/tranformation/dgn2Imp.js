3b39da79f73501227cae9c35b541bbbc41149c9a
or3stis <or3stis.man@gmail.com>
2017-10-21 21:09:04 +0100

    :sparkles: tranformation dgn -> imp

---
+++ app/src/tranformation/dgn2Imp.js
@@ -0,0 +1,32 @@
+// applies tranformation rules from design phase to implementation phase
+
+const save = require('../helpers/save.js')
+
+module.exports = function dgnState2ImpState (cy) {
+  cy.nodes().map(node => {
+    if (node.data().info.concept === 'micronet') {
+      node.data().info.state = ''
+    } else if (node.data().info.concept === 'thing') {
+      // const neighbor = node.neighborhood()
+      // neighbor.map(type => {
+      //   if (type.data().hasOwnProperty('info') === true) {
+      //     if (type.data().info.concept === 'net') {
+      //       node.data().label = 'undefined node'
+      //       node.data().info.concept = 'undefined node'
+      //     }
+      //   }
+      // })
+      if (node.data().info.component === 'hardware') {
+        node.data().label = 'device'
+        node.data().info.concept = 'device'
+      } else if (node.data().info.component === 'software') {
+        node.data().label = 'application'
+        node.data().info.concept = 'application'
+      }
+    } else if (node.data().info.concept === 'sensor') {
+      node.data().label = 'control sensor'
+      node.data().info.concept = 'control sensor'
+    }
+  })
+  save(cy)
+}
