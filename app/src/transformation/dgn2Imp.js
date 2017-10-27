640a1c598e89a0242fdbaa0596399f4fdb4da898
or3stis <or3stis.man@gmail.com>
2017-10-27 21:57:48 +0100

    update transformation rules

--- app/src/transformation/dgn2Imp.js
+++ app/src/transformation/dgn2Imp.js
@@ -9,26 +9,30 @@ module.exports = function dgnState2ImpState (cy) {
   cy.nodes().map(node => {
     if (node.data().info.concept === 'micronet') {
       node.data().info.state = ''
+    } else if (node.data().info.concept === 'unidentified node') {
+      node.data().label = 'unidentified node'
+      node.data().info.service = ''
+      node.data().info.input = ''
+      node.data().info.output = ''
+      node.data().info.concept = 'unidentified node'
     } else if (node.data().info.concept === 'thing') {
-      // const neighbor = node.neighborhood()
-      // neighbor.map(type => {
-      //   if (type.data().hasOwnProperty('info') === true) {
-      //     if (type.data().info.concept === 'net') {
-      //       node.data().label = 'undefined node'
-      //       node.data().info.concept = 'undefined node'
-      //     }
-      //   }
-      // })
       if (node.data().info.component === 'hardware') {
         node.data().label = 'device'
+        node.data().info.layer = ''
+        node.data().info.type = ''
+        node.data().info.service = ''
+        node.data().info.input = ''
+        node.data().info.output = ''
+        node.data().info.update = ''
         node.data().info.concept = 'device'
       } else if (node.data().info.component === 'software') {
         node.data().label = 'application'
+        node.data().info.version = ''
+        node.data().info.update = ''
         node.data().info.concept = 'application'
       }
-    } else if (node.data().info.concept === 'sensor') {
-      node.data().label = 'control sensor'
-      node.data().info.concept = 'control sensor'
+    } else if (node.data().info.concept === 'information') {
+      node.data().info.location = ''
     }
   })
   save(cy)
