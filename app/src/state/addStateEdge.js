ade634fb6c12238a7b8a453cb10f283852469a1b
or3stis <or3stis.man@gmail.com>
2017-11-27 20:20:00 +0000

    :bug: fix info->asto change

--- app/src/state/addStateEdge.js
+++ app/src/state/addStateEdge.js
@@ -3,11 +3,11 @@
 const printMessageText = require('../helpers/printMessageText.js')
 const addEdge = require('../core/addEdge.js')
 
-module.exports = function addStateComponent (cy, srcNode, trgNode) {
+module.exports = function addStateEdge (cy, srcNode, trgNode) {
   let srcNodeId = srcNode.id
   let trgNodeId = trgNode.id
-  let srcNodeCpt = srcNode.info.concept
-  let trgNodeCpt = trgNode.info.concept
+  let srcNodeCpt = srcNode.asto.concept
+  let trgNodeCpt = trgNode.asto.concept
 
   switch (true) {
     case srcNodeCpt === 'model' && trgNodeCpt === 'model':
