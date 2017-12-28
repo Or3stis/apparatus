523e62afb987c94f3e0247bdf8ab21d11c1eb1c4
or3stis <or3stis.man@gmail.com>
2017-12-28 20:46:22 +0000

    :hammer: refactor suggestion.js

--- app/src/imp/suggestion.js
+++ app/src/imp/suggestion.js
@@ -1,42 +1,38 @@
 // identifies insecure patterns in the models
 const printMsgTxt = require('../helpers/printMsgTxt.js')
 
-// const suggestionList = {
-//   s0: {
-//     concept: 'device',
-//     layer: 'perception',
-//     output: 'devices in the physical layer require physical security'
-//   }
-// }
+const list = {
+  s0: {
+    concept: 'device',
+    layer: 'perception',
+    suggestion: 'devices in the perception layer require physical security.'
+  },
+  s1: {
+    concept: 'network connection',
+    description: 'wireless',
+    suggestion:
+      'wireless connections are subject to information disclosure attacks. Use encrypted protocols.'
+  }
+}
 
 module.exports = function suggestion (cy) {
   cy.elements().addClass('faded')
 
   cy.nodes().map(node => {
-    if (
-      node.data().asto.concept === 'device' &&
-      node.data().asto.layer === 'perception'
-    ) {
+    let nd = node.data().asto
+    if (nd.concept === list.s0.concept && nd.layer === list.s0.layer) {
       node.removeClass('faded')
       node.addClass('attention')
-      printMsgTxt(
-        `${
-          node.data().id
-        }: devices in the perception layer require physical security.`
-      )
+      printMsgTxt(`${node.data().id}: ${list.s0.suggestion}`)
     }
 
     if (
-      node.data().asto.concept === 'network connection' &&
-      node.data().asto.description === 'wireless'
+      nd.concept === list.s1.concept &&
+      nd.description === list.s1.description
     ) {
       node.removeClass('faded')
       node.addClass('attention')
-      printMsgTxt(
-        `${
-          node.data().id
-        }: wireless connections are subject to information disclosure attacks. Use encrypted protocols.`
-      )
+      printMsgTxt(`${node.data().id}: ${list.s1.suggestion}`)
     }
   })
 }
