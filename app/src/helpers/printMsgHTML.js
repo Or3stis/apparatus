be30e1f8b9fccb183eca428bcb2b81953ddfd9b1
or3stis <or3stis.man@gmail.com>
2018-01-31 21:47:52 +0000

    add bubbles id

--- app/src/helpers/printMsgHTML.js
+++ app/src/helpers/printMsgHTML.js
@@ -1,13 +1,20 @@
-// helper to print HTML formatted text
+// helper to print HTML formatted text as message bubble
 
 const checkBubbles = require('./checkBubbles.js')
+
+// id couter for the bubbles
+let idCnt = 0
 
 module.exports = function printMsgHTML (toPrint) {
   const htmlElement = document.getElementById('message-area-id')
 
   // create message element
   const span = document.createElement('span')
+  idCnt += 1
+  span.id = `bubble-html-${idCnt}`
   span.className = 'bubble'
+
+  // add content to the bubble
   span.innerHTML = toPrint
   htmlElement.appendChild(span)
 
