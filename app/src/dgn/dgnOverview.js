fa0b18cd66a9e218b0855658700423a766f8f183
or3stis <or3stis.man@gmail.com>
2018-02-05 22:28:34 +0000

    stop counting the graph twice

--- app/src/dgn/dgnOverview.js
+++ app/src/dgn/dgnOverview.js
@@ -7,80 +7,107 @@ module.exports = function overview (cy) {
   const securityArray = dgnMetamodel.security
   const socialArray = dgnMetamodel.social
 
-  let result = ''
+  // initialize the output of the module
+  let output = ''
+
+  // object to store the number of each node in the graph
+  const graphNodes = {
+    network: {
+      numberOfNodes: 0
+    },
+    security: {
+      numberOfNodes: 0
+    },
+    social: {
+      numberOfNodes: 0
+    },
+    device: {
+      numberOfNodes: 0
+    },
+    application: {
+      numberOfNodes: 0
+    },
+    micronet: {
+      numberOfNodes: 0
+    },
+    net: {
+      numberOfNodes: 0
+    },
+    information: {
+      numberOfNodes: 0
+    },
+    asset: {
+      numberOfNodes: 0
+    },
+    constraint: {
+      numberOfNodes: 0
+    },
+    threat: {
+      numberOfNodes: 0
+    },
+    actor: {
+      numberOfNodes: 0
+    },
+    maliciousActor: {
+      numberOfNodes: 0
+    }
+  }
 
   const totalNodes = cy.elements().nodes().length
-  result = `• total nodes: ${totalNodes}\n\n`
+  output = `• total nodes: ${totalNodes}\n\n`
 
-  let networkNode = 0
-  let securityNode = 0
-  let socialNode = 0
-
+  // count the number of nodes
   cy.nodes().map(node => {
     const nodeConcept = node.data().asto.concept
+    // count the network nodes
     if (networkArray.includes(nodeConcept) === true) {
-      networkNode += 1
+      graphNodes.network.numberOfNodes += 1
+      if (nodeConcept === 'device') {
+        graphNodes.device.numberOfNodes += 1
+      } else if (nodeConcept === 'application') {
+        graphNodes.application.numberOfNodes += 1
+      } else if (nodeConcept === 'micronet') {
+        graphNodes.micronet.numberOfNodes += 1
+      } else if (nodeConcept === 'net') {
+        graphNodes.net.numberOfNodes += 1
+      } else if (nodeConcept === 'information') {
+        graphNodes.information.numberOfNodes += 1
+      }
+      // count the security nodes
     } else if (securityArray.includes(nodeConcept) === true) {
-      securityNode += 1
+      graphNodes.security.numberOfNodes += 1
+      if (nodeConcept === 'asset') {
+        graphNodes.asset.numberOfNodes += 1
+      } else if (nodeConcept === 'threat') {
+        graphNodes.threat.numberOfNodes += 1
+      } else if (nodeConcept === 'constraint') {
+        graphNodes.constraint.numberOfNodes += 1
+      } else if (nodeConcept === 'malicious actor') {
+        graphNodes.maliciousActor.numberOfNodes += 1
+      }
+      // count the social node
     } else if (socialArray.includes(nodeConcept) === true) {
-      socialNode += 1
-    }
-  })
-  result = `${result}• network nodes: ${networkNode}\n`
-  result = `${result}• social nodes: ${socialNode}\n`
-  result = `${result}• security nodes: ${securityNode}\n\n`
-
-  let deviceNode = 0
-  let applicationNode = 0
-  let micronetNode = 0
-  let netNode = 0
-  let informationNode = 0
-  let assetNode = 0
-  let threatNode = 0
-  let constraintNode = 0
-  let malActorNode = 0
-  let actorNode = 0
-
-  cy.nodes().map(node => {
-    const nodeConcept = node.data().asto.concept
-    if (nodeConcept === 'device') {
-      deviceNode += 1
-    } else if (nodeConcept === 'application') {
-      applicationNode += 1
-    } else if (nodeConcept === 'micronet') {
-      micronetNode += 1
-    } else if (nodeConcept === 'net') {
-      netNode += 1
-    } else if (nodeConcept === 'information') {
-      informationNode += 1
-    } else if (nodeConcept === 'asset') {
-      assetNode += 1
-    } else if (nodeConcept === 'threat') {
-      threatNode += 1
-    } else if (nodeConcept === 'constraint') {
-      constraintNode += 1
-    } else if (nodeConcept === 'malicious actor') {
-      malActorNode += 1
-    } else if (nodeConcept === 'actor') {
-      actorNode += 1
+      graphNodes.social.numberOfNodes += 1
+      if (nodeConcept === 'actor') {
+        graphNodes.actor.numberOfNodes += 1
+      }
     }
   })
 
-  result = `${result}• device nodes: ${deviceNode}\n`
-  result = `${result}• application nodes: ${applicationNode}\n`
-  result = `${result}• micronet nodes: ${micronetNode}\n`
-  result = `${result}• net nodes: ${netNode}\n`
-  result = `${result}• information nodes: ${informationNode}\n`
-  result = `${result}• asset nodes: ${assetNode}\n`
-  result = `${result}• threat nodes: ${threatNode}\n`
-  result = `${result}• constraint nodes: ${constraintNode}\n`
-  result = `${result}• malicious actor nodes: ${malActorNode}\n`
-  result = `${result}• actor nodes: ${actorNode}\n`
+  // construct the output final output
+  const conOutput = (node, numberOfNodes) => {
+    output = `${output}• ${node} nodes: ${numberOfNodes}\n`
+  }
 
-  // show result in the graph container
+  // loop the object to make the output
+  Object.keys(graphNodes).map(node => {
+    conOutput(node, graphNodes[node].numberOfNodes)
+  })
+
+  // show output in the graph container
   const containerNode = document.getElementById('container-node-id')
   const containerNodeInfo = document.getElementById('container-node-info-id')
   // appends info to the div
   containerNode.style.display = 'block'
-  containerNodeInfo.textContent = result
+  containerNodeInfo.textContent = output
 }
