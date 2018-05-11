23a7a457174bfef071d557fc8ea4875568aed8ac
or3stis <or3stis.man@gmail.com>
2018-05-11 21:20:30 +0100

    checks if bubbles are empty

--- app/src/helpers/bubbleHTML.js
+++ app/src/helpers/bubbleHTML.js
@@ -9,31 +9,34 @@ let idCnt = 0
  * @param {string} toPrint message to print in the bubble
  */
 module.exports = function bubbleHTML (toPrint) {
-  const msgArea = document.getElementById('message-area-id')
+  // if the input is empty it doesn't render an small empty bubble
+  if (toPrint !== '') {
+    const msgArea = document.getElementById('message-area-id')
 
-  // create bubble element
-  const bubble = document.createElement('span')
-  bubble.className = 'bubble'
-  idCnt += 1
-  bubble.id = `bubble-html-${idCnt}`
+    // create bubble element
+    const bubble = document.createElement('span')
+    bubble.className = 'bubble'
+    idCnt += 1
+    bubble.id = `bubble-html-${idCnt}`
 
-  // add content to the bubble
-  bubble.innerHTML = toPrint
-  msgArea.appendChild(bubble)
+    // add content to the bubble
+    bubble.innerHTML = toPrint
+    msgArea.appendChild(bubble)
 
-  // create the close button
-  const bubbleBtn = document.createElement('button')
-  bubbleBtn.className = 'bubble-btn'
-  bubbleBtn.id = `bubbleBtn-${idCnt}`
-  bubble.appendChild(bubbleBtn)
+    // create the close button
+    const bubbleBtn = document.createElement('button')
+    bubbleBtn.className = 'bubble-btn'
+    bubbleBtn.id = `bubbleBtn-${idCnt}`
+    bubble.appendChild(bubbleBtn)
 
-  // attach event listener to the close button
-  bubbleBtn.addEventListener('click', () => {
-    msgArea.removeChild(bubble)
-  })
+    // attach event listener to the close button
+    bubbleBtn.addEventListener('click', () => {
+      msgArea.removeChild(bubble)
+    })
 
-  msgArea.lastChild.scrollIntoView(false)
+    msgArea.lastChild.scrollIntoView(false)
 
-  // checks the number of bubbles
-  checkBubbles()
+    // checks the number of bubbles
+    checkBubbles()
+  }
 }
