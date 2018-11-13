f6f5eb942333ace106ab93a5737ca8eb96457d4a
or3stis <or3stis.man@gmail.com>
2018-11-13 13:26:35 +0200

    convert index.js and homeMenu.js to .ts

--- app/src/helpers/theme.ts
+++ app/src/helpers/theme.ts
@@ -1,7 +1,7 @@
 // module to change color themes between light and dark
 const { app } = require('electron').remote
 
-const graphStyle = require('../settings/graphStyle.js')
+import * as graphStyle from '../settings/graphStyle'
 
 const userDataPath = app.getPath('userData')
 const settings = require(`${userDataPath}/astoSettings.js`)
@@ -11,7 +11,7 @@ const settings = require(`${userDataPath}/astoSettings.js`)
  *
  * @param {string} color light or dark
  */
-const setTheme = color => {
+const setTheme = (color: string) => {
   // update color variables
   settings.setColors(color)
 
@@ -19,23 +19,18 @@ const setTheme = color => {
   document.documentElement.style.setProperty('--main-tx-color', settings.text)
   document.documentElement.style.setProperty(
     '--main-bg-color',
-    settings.background
+    settings.background,
   )
   document.documentElement.style.setProperty(
     '--main-dark-bg-color',
-    settings.darkBackground
+    settings.darkBackground,
   )
   document.documentElement.style.setProperty('--blue-color', settings.blue)
   document.documentElement.style.setProperty('--black-color', settings.black)
 }
 
-/**
- * changes the UI and graph colors
- *
- * @param {Object} cy cytoscape instance
- * @param {string} color light or dark
- */
-const setThemeGraph = (cy, color) => {
+/** changes the UI and graph colors */
+const setThemeGraph = (cy: any, color: string) => {
   // changes the UI theme
   setTheme(color)
 
@@ -44,7 +39,7 @@ const setThemeGraph = (cy, color) => {
   cy.style(graphStyle.style)
 }
 
-module.exports = {
+export {
   setTheme,
-  setThemeGraph
+  setThemeGraph,
 }
